b02e12c1c599a7b2f8d1a9480419eb2e90bd77fc
Astrionix <padalalmrreddy@gmail.com>
2025-12-28 17:08:30 +0530

    chore: remove MobileHeader and use ClientLayout

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -2,7 +2,7 @@
 
 import AndroidBackHandler from "@/components/AndroidBackHandler";
 import BiometricGuard from "@/components/BiometricGuard";
-import MobileHeader from "@/components/MobileHeader";
+
 import OfflineBanner from "@/components/OfflineBanner";
 import VoiceCommandCenter from "@/components/VoiceCommandCenter";
 
@@ -11,11 +11,6 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
         <BiometricGuard>
             <AndroidBackHandler />
             <OfflineBanner />
-            <MobileHeader onMenuClick={() => {
-                if (typeof window !== 'undefined') {
-                    window.dispatchEvent(new CustomEvent('toggleSidebar'));
-                }
-            }} />
             <VoiceCommandCenter />
             {children}
         </BiometricGuard>
